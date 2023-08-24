eca735b82d5815eb9baab61038289dccad4dc2c2
Julio Guella <jguella@kindalab.co>
2023-08-24 20:23:55 -0300

    add navbar and minimal template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,31 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-// Import your components/pages
 import Home from './pages/Home'
 import About from './pages/About';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 import Navbar from './components/Navbar/Navbar';
+import Login from './pages/Login';
+import { Container } from "react-bootstrap";
+import { MantineProvider } from '@mantine/core';
 
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route element={<NotFound />} />
-      </Routes>
-    </Router >
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Router>
+        <Navbar />
+        <Container className='d-flex flex-wrap'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route element={<NotFound />} />
+          </Routes>
+        </Container>
+      </Router >
+    </MantineProvider>
   );
 }
 
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,23 @@
-import { DatePicker } from '@mantine/dates';
+import { Card, Container, Row } from 'react-bootstrap';
+import DatePicker from '../../components/DatePicker/DatePicker';
 
 const Home = () => {
-    return (
-        <div>
-            <div>Home</div>
-            <DatePicker />
-        </div>
-    );
+  return (
+    <Container className='d-flex flex-wrap flex-column justify-content-center align-content-center pt-2'>
+      <Card className='d-flex justify-content-center h-100 w-100'>
+        <Card.Body>
+          <h1> Home </h1>
+          <Row className='d-flex justify-content-center'>
+            <Card style={{ height: '300px', width: '300px' }}>
+              <Card.Body>
+                <DatePicker />
+              </Card.Body>
+            </Card>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 }
 
 export default Home
