35c46399c3ce88423868d0d897df3c281aeb520b
Julio Guella <jguella@kindalab.co>
2023-10-16 13:50:17 -0300

    fix baseUrl

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'https://api-sivrg.methizul.com.ar:8443',
 });
 
 api.interceptors.request.use(
