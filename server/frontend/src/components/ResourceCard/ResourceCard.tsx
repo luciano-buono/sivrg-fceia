90c5e5bbbb7e53fe62259b253c556c4331e47376
Julio Guella <jguella@kindalab.co>
2023-10-25 19:50:59 -0300

    refactor home admin mobile

--- server/frontend/src/components/ResourceCard/ResourceCard.tsx
+++ server/frontend/src/components/ResourceCard/ResourceCard.tsx
@@ -1,4 +1,5 @@
-import { Center, RingProgress } from '@mantine/core';
+import { Center, Progress, RingProgress, Tooltip, em } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { FC, useState } from 'react';
 import { Button, Row } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
@@ -6,17 +7,26 @@ import { Card } from 'react-bootstrap';
 const ResourceCard: FC<{ initialQuantity: number; icon: string }> = ({ initialQuantity, icon }) => {
   const [quantity, setQuantity] = useState(initialQuantity);
 
-  return (
+  const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
+
+  return isMobile ? (
+    <>
+      <i className={icon} style={{ fontSize: '2em' }}></i>
+      <Progress color="orange" className="mt-2" value={quantity} />
+    </>
+  ) : (
     <Card style={{ width: 'auto' }}>
       <Card.Body>
-        <RingProgress
-          label={
-            <Center>
-              <i className={icon} style={{ fontSize: '3em' }}></i>
-            </Center>
-          }
-          sections={[{ value: quantity, color: 'orange' }]}
-        />
+        <Tooltip label={quantity}>
+          <RingProgress
+            label={
+              <Center>
+                <i className={icon} style={{ fontSize: '3em' }}></i>
+              </Center>
+            }
+            sections={[{ value: quantity, color: 'orange' }]}
+          />
+        </Tooltip>
         <Row style={{ width: 'auto' }} className="justify-content-center">
           <Button style={{ width: '40px' }} className="me-2" onClick={() => setQuantity((prev) => prev + 1)}>
             +
