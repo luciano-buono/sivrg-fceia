db7d81564751f07f1a88aabc3c01c4e453f2d2ef
Scaenicas <104804601+Scaenicas@users.noreply.github.com>
2024-02-18 19:25:14 -0300

    Add files via upload

--- client/client_pantalla-playa.py
+++ client/client_pantalla-playa.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 from  tkinter import *
 import tkinter as tk
-#import RPi.GPIO as GPIO
 import pygame
 from gtts import gTTS
   
@@ -56,27 +55,30 @@ menubar.add_cascade(label="Acciones", menu=filemenu)
 
 root.config(menu=menubar)
 
-# Seteo de pin
-ONAIRPIN=21
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(ONAIRPIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
+#FUNCION QUE ACTUALIZA PATENTES
+def cambio_patente():
+    texto_patente= "AAA333"
+    num_silo= "2"
+    texto_prox_patente = "CCC555"
+    patente.config(text=texto_patente)
+    silo.config(text="Silo n°"+ num_silo)
+    prox_patente.configure(text="Próximo:  "+ texto_prox_patente)
+    patente.after(200,cambio_patente)
+    silo.after(200,cambio_patente)
+    prox_patente.after(200,cambio_patente)
 
 #root.geometry("1280x720")
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 root.configure(background='black')
 root.title("SIVRG")
-clock=Label(root,font=("times",50,"bold"))
-#clock.grid(row=2,column=1,pady=200,padx=350)
 
 
-# if(GPIO.input(ONAIRPIN)==True):
-patente=Label(root,text=texto_patente,font="Arial 300 bold",fg="red")
+patente=Label(root,font="Arial 300 bold",fg="red")
 patente.configure(bg='black')
 silo=Label(root,font=("times",50,"bold"))
-silo.config(text="Silo n°"+ num_silo,bg="black",fg="green",font="Arial 220 bold")
+silo.config(bg="black",fg="green",font="Arial 220 bold")
 prox_patente=Label(root,font=("times",50,"bold"))
-prox_patente.configure(text="Próximo:  "+ texto_prox_patente,bg="black",fg="yellow",font="Arial 100 bold")
+prox_patente.configure(bg="black",fg="yellow",font="Arial 100 bold")
 
 
 patente.place(relx=0.5,rely=0.2,anchor='center')
@@ -84,4 +86,5 @@ silo.place(relx=0.5,rely=0.5,anchor='center')
 prox_patente.place(relx=0.5,rely=0.8,anchor='center')
 
 
+cambio_patente()
 root.mainloop()
