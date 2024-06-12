77784e3ad7f434bec403b0fe0c2f25fbe1318d27
Julio Guella <jguella@kindalab.co>
2024-06-11 22:06:00 -0300

    wip monitor and reservado

--- sivrg/frontend/src/pages/AdminPage/AdminPage.tsx
+++ sivrg/frontend/src/pages/AdminPage/AdminPage.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router';
 import { ProtectedRoutes } from '../../App';
 import HomeAdmin from '../HomeAdmin';
 import ReservationsAdminPage from '../ReservationsPage';
+import MonitorPage from '../MonitorPage';
 
 const AdminPage = () => {
   return (
@@ -13,6 +14,7 @@ const AdminPage = () => {
             <Route index element={<HomeAdmin />} />
             <Route path="/reservations" element={<ReservationsAdminPage filterByDay={false} />} />
             <Route path="/reservations/current" element={<ReservationsAdminPage filterByDay />} />
+            <Route path="/monitor" element={<MonitorPage />} />
           </Route>
         </Routes>
       </Container>
--- sivrg/frontend/src/pages/MonitorPage/MonitorPage.tsx
+++ sivrg/frontend/src/pages/MonitorPage/MonitorPage.tsx
@@ -1,28 +1,58 @@
-import { useQuery } from '@tanstack/react-query';
-import { Turno } from '../../types';
-import api from '../../api';
-import useSession from '../../hooks/useSession';
+// import { useQuery } from '@tanstack/react-query';
+// import { Turno } from '../../types';
+// import api from '../../api';
+import { Button, Divider } from '@mantine/core';
+import { useFullscreen } from '@mantine/hooks';
+import { Row } from 'react-bootstrap';
 
 const MonitorPage = () => {
   const today = new Date();
   const yesterday = new Date(today);
   yesterday.setDate(today.getDate() - 1);
-  const { logout } = useSession();
 
-  const queryTurno = useQuery<Turno[]>({
-    queryKey: ['turnos'],
-    queryFn: () =>
-      api.get(`/turnos/?start_date=${yesterday.toISOString()}&end_date=${today.toISOString()}`).then((res) => res.data),
-  });
+  const { ref, toggle, fullscreen } = useFullscreen();
 
-  const { data: turnos } = queryTurno;
+  // @TODO: FILTRAR CON status=in_progress_entrada       accepted, 10 turnos
+  // sorted by checking_time
+
+  // const queryTurno = useQuery<Turno[]>({
+  //   queryKey: ['turnos'],
+  //   queryFn: () =>
+  //     api.get(`/turnos/?start_date=${yesterday.toISOString()}&end_date=${today.toISOString()}`).then((res) => res.data),
+  // });
+
+  const mockedTurnos = [
+    { vehiculo: { patente: 'AB523K1' }, chofer: { nombre: 'juan', apellido: 'perez' }, empresa: { nombre: 'empresa' } },
+    { vehiculo: { patente: 'AB223K2' }, chofer: { nombre: 'juan', apellido: 'perez' }, empresa: { nombre: 'empresa' } },
+    { vehiculo: { patente: 'AB525K1' }, chofer: { nombre: 'juan', apellido: 'perez' }, empresa: { nombre: 'empresa' } },
+    { vehiculo: { patente: 'A5523K1' }, chofer: { nombre: 'juan', apellido: 'perez' }, empresa: { nombre: 'empresa' } },
+    { vehiculo: { patente: 'AB523K1' }, chofer: { nombre: 'juan', apellido: 'perez' }, empresa: { nombre: 'empresa' } },
+    { vehiculo: { patente: 'AB223K2' }, chofer: { nombre: 'juan', apellido: 'perez' }, empresa: { nombre: 'empresa' } },
+    { vehiculo: { patente: 'AB525K1' }, chofer: { nombre: 'juan', apellido: 'perez' }, empresa: { nombre: 'empresa' } },
+    { vehiculo: { patente: 'A5523K1' }, chofer: { nombre: 'juan', apellido: 'perez' }, empresa: { nombre: 'empresa' } },
+    { vehiculo: { patente: 'AB523K1' }, chofer: { nombre: 'juan', apellido: 'perez' }, empresa: { nombre: 'empresa' } },
+    { vehiculo: { patente: 'AB223K2' }, chofer: { nombre: 'juan', apellido: 'perez' }, empresa: { nombre: 'empresa' } },
+  ];
+
+  // const { data: turnos } = queryTurno;
   return (
     <>
-      <div className="d-flex bg-primary vh-100">
-        <div className="fw-bold fs-1">TURNOS: </div>
-        {turnos?.map((x) => <div>{x.vehiculo.patente}</div>)}
+      <div className="d-flex vh-100 vw-100 flex-column" style={{ backgroundColor: '#228be6' }} ref={ref}>
+        <div>
+        <Button onClick={toggle} color={'blue'} hidden={fullscreen}>
+          Pantalla completa
+      </Button>
+          <div className="fw-bold fs-1 d-flex text-black ps-2">TURNOS EN CURSO</div>
+          <Divider my={6} />
+          <ol className="d-flex flex-column" style={{ listStyle: 'none' }}>
+            {mockedTurnos?.map((x) => (
+              <Row className="h3 d-flex text-black">
+                <li>{`${x.vehiculo.patente} - ${x.chofer.apellido} ${x.chofer.nombre} - ${x.empresa.nombre}`}</li>
+              </Row>
+            ))}
+          </ol>
+        </div>
       </div>
-      <button onClick={() => logout()}></button>
     </>
   );
 };
